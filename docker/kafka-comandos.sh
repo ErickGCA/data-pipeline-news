0be2bab58031d6bdcb8fcfe1cb82c0b003a5834a
Erick <erick.cabral122@gmail.com>
2025-04-15 22:52:20 -0300

    gitignore updates

--- docker/kafka-comandos.sh
+++ docker/kafka-comandos.sh
@@ -1,12 +1,18 @@
-# Criar tópico
+
 kafka-topics --create --topic meu-topico-test --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
 
-# Listar tópicos
 kafka-topics --list --bootstrap-server localhost:9092
 
-# Produzir mensagens
 kafka-console-producer --topic meu-topico-test --bootstrap-server localhost:9092
 
-# Consumir mensagens
 kafka-console-consumer --topic meu-topico-test --bootstrap-server localhost:9092 --from-beginning
 
+CD ~/pipelines-news
+
+mkdir  (create diretory)
+
+touch (create files)
+
+cd .. (initial repository)
+
+code . (open vscode)

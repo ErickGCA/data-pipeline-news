bdd0f7826def5cf1b8f929530b073ab5c4d9d7d2
Erick <erick.cabral122@gmail.com>
2025-04-16 19:33:11 -0300

    starting to implement aws s3

---
+++ scripts/load_to_s3.py
@@ -0,0 +1,26 @@
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def upload_to_s3(file_path, bucket_name, object_name):
+    s3 = boto3.client(
+        's3',
+        aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+        region_name=os.getenv("AWS_REGION")
+    )
+
+    try:
+        s3.upload_file(file_path, bucket_name, object_name)
+        print(f"Arquivo '{file_path}' enviado com sucesso para '{bucket_name}/{object_name}'")
+    except Exception as e:
+        print(f"Erro ao enviar para o S3: {e}")
+
+if __name__ == "__main__":
+    file_path = "../pipelines-news/data/filtered_news.json"
+    bucket_name = os.getenv("S3_BUCKET_NAME")
+    object_name = "filtered_news.json"
+
+    upload_to_s3(file_path, bucket_name, object_name)
